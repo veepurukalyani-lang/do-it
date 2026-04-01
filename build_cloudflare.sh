49ef0f590ce0de4f95bcaea941538ee3898c9cbd
Cloudflare Deploy <dev@cloudflare.com>
2026-04-01 22:12:11 +0530

    updated Flutter SDK version in build script

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1. Download Flutter
-FLUTTER_VERSION="3.29.0"
+# 1. Download Flutter (Updated to 3.41.6 to match your SDK requirement)
+FLUTTER_VERSION="3.41.6"
 echo "Downloading Flutter $FLUTTER_VERSION..."
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 
